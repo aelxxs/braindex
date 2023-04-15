61aef7ac8246f8dfe371e24584af2461a2bcc72a
alexis <alexism.ng26@gmail.com>
2023-04-14 22:43:52 -0500

    feat: ui init and react-nav

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { TamaguiProvider, Button } from "tamagui";
+import config from "./tamagui.config";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<TamaguiProvider config={config}>
+			<View style={styles.container}>
+				<Button>
+					<Text>Hello</Text>
+				</Button>
+				<StatusBar style="auto" />
+			</View>
+		</TamaguiProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
