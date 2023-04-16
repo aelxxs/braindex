4722ded8676b1d62d92f1f1ffc4751f9b1c897f0
Alexis Vielma <alexis.vielma.us@gmail.com>
2023-04-16 06:12:46 -0500

    feat: remove unused components

--- src/components/create/Create.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from "react-native";
-
-export const CreateModal = () => {
-return(
-<Text>Hello</Text>
-); 
-}
--- src/components/nav/Nav.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from "react-native";
-
-export const Navigation = () => {
-return(
-<Text>Hello</Text>
-); 
-}
